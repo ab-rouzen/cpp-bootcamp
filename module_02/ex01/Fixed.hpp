98c53c6074487c8881a6eba004c2c77a85b6dc9e
abed <abed-rwx@proton.me>
2023-02-27 20:30:22 +0100

    Add: ex01 - Fixed point representation and conversions

---
+++ module_02/ex01/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef	FIXED_H
+#define	FIXED_H
+
+#include <iostream>
+
+class Fixed
+{
+	public:
+		Fixed(void);
+		Fixed(const Fixed &f);
+		Fixed(const int &in);
+		Fixed(const float &fn);
+		~Fixed(void);
+		Fixed &operator=(const Fixed &f);
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void)	const; 
+	
+	private:
+		int					number;
+		static const int	frac = 8;
+};
+
+std::ostream &operator<<(std::ostream &buf, const Fixed &f);
+
+#endif	// FIXED_H
