f543cc1d5f5b5056b399e85cbc1326cefdfc1dba
abed <abed-rwx@proton.me>
2023-05-07 16:59:22 +0100

    Add: ex02 - draft

---
+++ module_09/ex02/main.cpp
@@ -0,0 +1,77 @@
+#include "PmergeMe.hpp"
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+template<class C>
+void	print_numbers(C container_type)
+{
+	typename C::const_iterator	itb = container_type.begin();	
+	typename C::const_iterator	ite = container_type.end();
+	for (; itb != ite; itb++)
+		std::cout << *itb << " ";
+	std::cout << std::endl;
+}
+
+void	insert_sort(std::vector<int> &vec);
+void	merge_sort(std::vector<int> &vec);
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+		return (1);
+	
+	std::vector<int>	c1;
+	std::stringstream	is(argv[1]);
+	int	tmpVal;
+	while (is >> tmpVal)
+	{
+		c1.push_back(tmpVal);
+	}
+	if (is.bad()) // check for bad input?
+		std::__throw_invalid_argument("Invalid input.");
+	// insert_sort(c1);
+	print_numbers(c1);
+	merge_sort(c1);
+	std::cout << std::endl;
+	return 0;
+}
+
+void	insert_sort(std::vector<int> &vec)
+{
+	for (int i = 0; i != vec.size(); i++)
+	{
+		for(int	j = 0; j != i; j++)
+		{
+			if (vec[i] < vec[j])
+			{
+				int	tmp = vec[i];
+				vec.erase(vec.begin() + i);
+				vec.insert(vec.begin() + j, tmp);
+				break;
+			}
+		}
+	}
+}
+
+void	merge_sort(std::vector<int> &vec)
+{
+	if (vec.size() == 1)
+	{
+		std::cout << vec[0] << std::endl;
+		return ;
+	}
+	int	mid = vec.size() / 2;
+	std::cout << "mid is " << mid << std::endl;
+	std::vector<int>	left(mid);
+	std::vector<int>	right(vec.size() - mid);
+	for(int i = 0; i < mid; i++)
+		left.push_back(vec[i]);
+	for(int i = mid; i < vec.size(); i++)
+		right.push_back(vec[i]);
+	std::cout << "left size " << left.size() << std::endl;
+	print_numbers(left);
+	merge_sort(left);
+	std::cout << "to right" << std::endl;
+	merge_sort(right);
+}
